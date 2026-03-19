86aa3e47daf7993eb2408c92c56dfe195473d6d0
mei <meisrikw@gmail.com>
2026-03-19 11:21:04 +0700

    Update soalTugas1.cpp

--- soalTugas1.cpp
+++ soalTugas1.cpp
@@ -1,17 +1,53 @@
-/*
-Capt Dodo memiliki beberapa pilihan transportasi untuk bepergian dari tempat magang ke kampus dan kembali lagi:
+#include <iostream>
 
-- Bus Hijau: Biaya Rp5.000. Beroperasi dari pukul 7 -18.
-- Bus Linus : Gratis, tetapi hanya tersedia pada pukul 6 - 8 dan 15 - 17.
-- Angkot: Biaya Rp10.000. Selalu tersedia tanpa batasan waktu.
+using namespace std;
 
-Karena Capt dodo adalah seorang anak kos, ia ingin menghemat pengeluaran transportasi semaksimal mungkin. 
+int main() {
+    int jamBerangkat, jamPulang;
+    int biayaTotal = 0;
 
-Diberikan dua input, yaitu:
-Jam keberangkatan (bilangan bulat 1-24)
-Jam kepulangan (bilangan bulat 1-24)
+    // input jam keberangkatan dan kepulangan user 
+    cout << "Jam Berangkat (1-24): ";
+    cin >> jamBerangkat;
+    cout << "Jam Pulang (1-24): ";
+    cin >> jamPulang;
 
-Buatlah program untuk menentukan moda transportasi yang dipilih untuk berangkat dan pulang dengan total biaya seminimal mungkin. 
-*/
+    cout << "\n--- Rencana Perjalanan ---" << endl;
 
-// Hapus komentar yang berisi soal sebelum memasukkan kode untuk jawaban
+    /*logika pemilihan transportasi 
+    kita harus mengecek bus linus karena biayanya gratis, jika tidak masuk jadwal linus baru cek bus hijau
+    jika keduanya tidak tersedia otomatis menggunakan angkot*/
+    cout << "Berangkat: ";
+    // cek apakah jam berangkat masuk jadwal bus linus
+    if ((jamBerangkat >= 6 && jamBerangkat <= 8) || (jamBerangkat >= 15 && jamBerangkat <= 17)) {
+        cout << "Bus Linus (Gratis)" << endl;
+        biayaTotal += 0;
+    // jika bukan linus, cek apakah masuk jadwal bus hijau
+    } else if (jamBerangkat >= 7 && jamBerangkat <= 18) {
+        cout << "Bus Hijau (Rp5.000)" << endl;
+        biayaTotal += 5000;
+
+    // jika diluar jadwal kedua bus diatas, maka naik angkot
+    } else {
+        cout << "Angkot (Rp10.000)" << endl;
+        biayaTotal += 10000;
+    }
+
+    cout << "Pulang: ";
+    // logika yang sama di terapkan untuk jam pulang
+    if ((jamPulang >= 6 && jamPulang <= 8) || (jamPulang >= 15 && jamPulang <= 17)) {
+        cout << "Bus Linus (Gratis)" << endl;
+        biayaTotal += 0;
+    } else if (jamPulang >= 7 && jamPulang <= 18) {
+        cout << "Bus Hijau (Rp5.000)" << endl;
+        biayaTotal += 5000;
+    } else {
+        cout << "Angkot (Rp10.000)" << endl;
+        biayaTotal += 10000;
+    }
+
+    cout << "--------------------------" << endl;
+    cout << "Total Biaya: Rp" << biayaTotal << endl;
+
+    return 0;
+}
